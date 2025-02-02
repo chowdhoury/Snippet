af51e375093114b37ce5d1f800cf3fa6c6c3d27c
Md Raihan Chowdhoury <mdr8077@gmail.com>
2025-02-02 14:54:47 +0600

    Create sublimesnippet.cpp

---
+++ sublimesnippet.cpp
@@ -0,0 +1,30 @@
+<snippet>
+    <content><![CDATA[
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\\n"
+#define pb push_back
+#define FastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void ${1:chowdhoury}()
+{
+    ${2:// Your code here}
+}
+
+signed main()
+{
+    FastIO();
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        ${1:chowdhoury}();
+    }
+    return 0;
+}
+    ]]></content>
+    <tabTrigger>lol</tabTrigger>
+    <scope>source.c++</scope>
+</snippet>
