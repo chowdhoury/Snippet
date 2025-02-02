853d830408f437738f204fd5da30b66b5e912255
Md Raihan Chowdhoury <mdr8077@gmail.com>
2025-02-02 14:18:12 +0600

    Create vscodesnippet.cpp

---
+++ vscodesnippet.cpp
@@ -0,0 +1,32 @@
+{
+    "Template for Competitive Programming": {
+        "prefix": "lol",
+        "body": [
+            "#include <bits/stdc++.h>",
+            "using namespace std;",
+            "#define int long long",
+            "#define endl \"\\n\"",
+            "#define pb push_back",
+            "#define FastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);",
+            "",
+            "void chowdhoury()",
+            "{",
+            "    ",
+            "}",
+            "",
+            "signed main()",
+            "    {",
+            "    FastIO();",
+            "    int t;",
+            "    t=1;",
+            "    // cin >> t;",
+            "    while (t--)",
+            "    {",
+            "        chowdhoury();",
+            "    }",
+            "    return 0;",
+            "}"
+        ],
+        "description": "Competitive Programming Template with FastIO and Custom Function"
+    }
+}
