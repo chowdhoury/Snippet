1dfcc3aa60fdd60c2fac824d0435def827b27400
Md Raihan Chowdhoury <mdr8077@gmail.com>
2025-02-02 14:17:26 +0600

    Create Text.cpp

---
+++ Text.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+#define pb push_back
+#define FastIO() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void chowdhoury()
+{
+    
+}
+
+signed main()
+    {
+    FastIO();
+    int t;
+    t=1;
+    // cin >> t;
+    while (t--)
+    {
+        chowdhoury();
+    }
+    return 0;
+}
